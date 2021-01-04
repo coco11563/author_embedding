b1542470037fcafb640393bda8a1e276adc6e3b4
coco11563@yeah.net <coco11563@yeah.net>
2021-01-04 15:43:00 +0800

    add gcn part

---
+++ code/model/model.py
@@ -0,0 +1,307 @@
+"""
+Modeling Relational Data with Graph Convolutional Networks
+Paper: https://arxiv.org/abs/1703.06103
+Code: https://github.com/MichSchli/RelationPrediction
+
+Difference compared to MichSchli/RelationPrediction
+* Report raw metrics instead of filtered metrics.
+* By default, we use uniform edge sampling instead of neighbor-based edge
+  sampling used in author's code. In practice, we find it achieves similar MRR
+  probably because the model only uses one GNN layer so messages are propagated
+  among immediate neighbors. User could specify "--edge-sampler=neighbor" to switch
+  to neighbor-based edge sampling.
+"""
+
+import argparse
+import pickle
+
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+from dgl.data.knowledge_graph import load_data
+from dgl.nn.pytorch import RelGraphConv
+import sys
+sys.path.append('/home/xiaomeng/jupyter_base/KGE_RGCN_PAPER_CODE')
+from code.utils.sample_utils import *
+from code.utils.model_utils import BaseRGCN
+from code.utils.gcn_utils import get_adj_and_degrees, build_test_graph, generate_sampled_graph_and_labels, calc_mrr
+
+
+class EmbeddingLayer(nn.Module):
+    def __init__(self, num_nodes, h_dim, embed_dict_path = "None", freeze_option = "freeze", transform_to_hdim = True):
+        super(EmbeddingLayer, self).__init__()
+        self.embed_dict_path = embed_dict_path
+        self.transfrom_to_hdim = transform_to_hdim
+        if embed_dict_path != "None" : # initialize with pretrain
+            with open(embed_dict_path, 'rb') as file:
+                embed_dict = pickle.load(file)
+                embed_li = []
+                for i in range(num_nodes) :
+                    embed_li.append(embed_dict[i])
+                embed_li = np.asarray(embed_li)
+                embed_li = torch.from_numpy(embed_li)
+                if freeze_option == "freeze" :
+                    self.embedding = torch.nn.Embedding.from_pretrained(embed_li)
+                elif freeze_option == "no freeze" :
+                    self.embedding = torch.nn.Embedding.from_pretrained(embed_li, False)
+                if transform_to_hdim : # init transform matrix
+                    self.transform_matrix = torch.nn.Parameter(torch.randn((self.embedding.weight.shape[1], h_dim)))
+                    nn.init.xavier_uniform_(self.transform_matrix,
+                                            gain=nn.init.calculate_gain('relu'))
+                    self.transform_bias = torch.nn.Parameter(torch.randn((num_nodes, 1)))
+                    nn.init.xavier_uniform_(self.transform_bias,
+                                            gain=nn.init.calculate_gain('relu'))
+
+        else :
+            print("init from random embedding")
+            self.embedding = torch.nn.Embedding(num_nodes, h_dim)
+
+    def forward(self, g, h, r, norm):
+        if self.embed_dict_path == "None" or not self.transfrom_to_hdim : # no pretrain
+            return self.embedding(h.squeeze())
+        else :
+            matrix_pre = self.embedding(h.squeeze())
+            return F.relu(matrix_pre.mm(self.transform_matrix) + self.transform_bias[h.squeeze()])
+
+class RGCN(BaseRGCN):
+    def build_input_layer(self):
+        return EmbeddingLayer(self.num_nodes, self.h_dim, self.from_pretrain, self.pretrain_option, self.transform_option)
+
+    def build_hidden_layer(self, idx):
+        act = F.relu if idx < self.num_hidden_layers - 1 else None
+        return RelGraphConv(self.h_dim, self.h_dim, self.num_rels, self.regularizer,
+                self.num_bases, activation=act, self_loop=True,
+                dropout=self.dropout)
+
+class LinkPredict(nn.Module):
+    def __init__(self, in_dim, h_dim, num_rels, regularizer = "bdd", num_bases=-1,
+                 num_hidden_layers=1, dropout=0, use_cuda=False, reg_param=0, from_pretrain = "None", pretrain_option = "freeze",
+                 transform_option = True):
+        super(LinkPredict, self).__init__()
+        self.rgcn = RGCN(in_dim, h_dim, h_dim, num_rels * 2, num_bases,
+                         num_hidden_layers, dropout, use_cuda,
+                         from_pretrain = from_pretrain,
+                         pretrain_option = pretrain_option,
+                         transform_option = transform_option,
+                         regularizer = regularizer)
+        self.reg_param = reg_param
+        self.w_relation = nn.Parameter(torch.Tensor(num_rels, h_dim))
+        nn.init.xavier_uniform_(self.w_relation,
+                                gain=nn.init.calculate_gain('relu'))
+
+    def calc_score(self, embedding, triplets):
+        # DistMult
+        s = embedding[triplets[:,0]]
+        r = self.w_relation[triplets[:,1]]
+        o = embedding[triplets[:,2]]
+        score = torch.sum(s * r * o, dim=1)
+        return score
+
+    def forward(self, g, h, r, norm):
+        return self.rgcn.forward(g, h, r, norm)
+
+    def regularization_loss(self, embedding):
+        return torch.mean(embedding.pow(2)) + torch.mean(self.w_relation.pow(2))
+
+    def get_loss(self, g, embed, triplets, labels):
+        # triplets is a list of data samples (positive and negative)
+        # each row in the triplets is a 3-tuple of (source, relation, destination)
+        score = self.calc_score(embed, triplets)
+        predict_loss = F.binary_cross_entropy_with_logits(score, labels)
+        reg_loss = self.regularization_loss(embed)
+        return predict_loss + self.reg_param * reg_loss
+
+def node_norm_to_edge_norm(g, node_norm):
+    g = g.local_var()
+    # convert to edge norm
+    g.ndata['norm'] = node_norm
+    g.apply_edges(lambda edges : {'norm' : edges.dst['norm']})
+    return g.edata['norm']
+
+def main(args):
+    # load graph data
+    global pool
+    data = load_data(args.dataset)
+    num_nodes = data.num_nodes
+    train_data = data.train
+    valid_data = data.valid
+    test_data = data.test
+    num_rels = data.num_rels
+
+    # check cuda
+    use_cuda = args.gpu >= 0 and torch.cuda.is_available()
+    if use_cuda:
+        torch.cuda.set_device(args.gpu)
+
+    # create model
+    model = LinkPredict(num_nodes,
+                        args.n_hidden,
+                        num_rels,
+                        num_bases=args.n_bases,
+                        num_hidden_layers=args.n_layers,
+                        dropout=args.dropout,
+                        use_cuda=use_cuda,
+                        reg_param=args.regularization)
+
+    # validation and testing triplets
+    valid_data = torch.LongTensor(valid_data)
+    test_data = torch.LongTensor(test_data)
+
+    # build test graph
+    test_graph, test_rel, test_norm = build_test_graph(
+        num_nodes, num_rels, train_data)
+    test_deg = test_graph.in_degrees(
+                range(test_graph.number_of_nodes())).float().view(-1,1)
+    test_node_id = torch.arange(0, num_nodes, dtype=torch.long).view(-1, 1)
+    test_rel = torch.from_numpy(test_rel)
+    test_norm = node_norm_to_edge_norm(test_graph, torch.from_numpy(test_norm).view(-1, 1))
+
+    if use_cuda:
+        model.cuda()
+
+    # build adj list and calculate degrees for sampling
+    adj_list, degrees = get_adj_and_degrees(num_nodes, train_data)
+
+    # optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    model_state_file = 'model_state.pth'
+    forward_time = []
+    backward_time = []
+    if args.edge_sampler == "neighbor" :
+        pool = MultiThreadSamplerPool(train_data, args.graph_batch_size, args.graph_split_size,
+                    num_rels, adj_list, degrees, args.negative_sample,
+                    args.edge_sampler, max_runtime=args.n_epochs, max_worker=64)
+    print('init sampler thread pool')
+    # training loop
+    print("start training...")
+
+    epoch = 0
+    best_mrr = 0
+    while True:
+        model.train()
+        epoch += 1
+        if args.edge_sampler == "neighbor" :
+            # 防止队列为空
+            while pool.q.empty():
+                pass
+            # perform edge neighborhood sampling to generate training graph and data
+            g, node_id, edge_type, node_norm, data, labels = pool.get()
+        # perform edge neighborhood sampling to generate training graph and data
+        else :
+            g, node_id, edge_type, node_norm, data, labels = \
+                generate_sampled_graph_and_labels(
+                    train_data, args.graph_batch_size, args.graph_split_size,
+                    num_rels, adj_list, degrees, args.negative_sample,
+                    args.edge_sampler)
+            print("Done edge sampling")
+
+        # set node/edge feature
+        node_id = torch.from_numpy(node_id).view(-1, 1).long()
+        edge_type = torch.from_numpy(edge_type)
+        edge_norm = node_norm_to_edge_norm(g, torch.from_numpy(node_norm).view(-1, 1))
+        data, labels = torch.from_numpy(data), torch.from_numpy(labels)
+        deg = g.in_degrees(range(g.number_of_nodes())).float().view(-1, 1)
+        if use_cuda:
+            node_id, deg = node_id.cuda(), deg.cuda()
+            edge_type, edge_norm = edge_type.cuda(), edge_norm.cuda()
+            data, labels = data.cuda(), labels.cuda()
+            g = g.to(args.gpu)
+
+        t0 = time.time()
+        embed = model(g, node_id, edge_type, edge_norm)
+        loss = model.get_loss(g, embed, data, labels)
+        t1 = time.time()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), args.grad_norm) # clip gradients
+        optimizer.step()
+        t2 = time.time()
+
+        forward_time.append(t1 - t0)
+        backward_time.append(t2 - t1)
+        print("Epoch {:04d} | Loss {:.4f} | Best MRR {:.4f} | Forward {:.4f}s | Backward {:.4f}s".
+              format(epoch, loss.item(), best_mrr, forward_time[-1], backward_time[-1]))
+
+        optimizer.zero_grad()
+
+        # validation
+        if epoch % args.evaluate_every == 0:
+            # perform validation on CPU because full graph is too large
+            if use_cuda:
+                model.cpu()
+            model.eval()
+            print("start eval")
+            embed = model(test_graph, test_node_id, test_rel, test_norm)
+            mrr = calc_mrr(embed, model.w_relation, torch.LongTensor(train_data),
+                                 valid_data, test_data, hits=[1, 3, 10], eval_bz=args.eval_batch_size,
+                                 eval_p=args.eval_protocol)
+            # save best model
+            if mrr < best_mrr:
+                if epoch >= args.n_epochs:
+                    break
+            else:
+                best_mrr = mrr
+                torch.save({'state_dict': model.state_dict(), 'epoch': epoch},
+                           model_state_file)
+            if use_cuda:
+                model.cuda()
+
+    print("training done")
+    print("Mean forward time: {:4f}s".format(np.mean(forward_time)))
+    print("Mean Backward time: {:4f}s".format(np.mean(backward_time)))
+
+    print("\nstart testing:")
+    # use best model checkpoint
+    checkpoint = torch.load(model_state_file)
+    if use_cuda:
+        model.cpu() # test on CPU
+    model.eval()
+    model.load_state_dict(checkpoint['state_dict'])
+    print("Using best epoch: {}".format(checkpoint['epoch']))
+    embed = model(test_graph, test_node_id, test_rel, test_norm)
+    calc_mrr(embed, model.w_relation, torch.LongTensor(train_data), valid_data,
+                   test_data, hits=[1, 3, 10], eval_bz=args.eval_batch_size, eval_p=args.eval_protocol)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='RGCN')
+    parser.add_argument("--dropout", type=float, default=0.2,
+            help="dropout probability")
+    parser.add_argument("--n-hidden", type=int, default=500,
+            help="number of hidden units")
+    parser.add_argument("--gpu", type=int, default=0,
+            help="gpu")
+    parser.add_argument("--lr", type=float, default=1e-2,
+            help="learning rate")
+    parser.add_argument("--n-bases", type=int, default=100,
+            help="number of weight blocks for each relation")
+    parser.add_argument("--n-layers", type=int, default=2,
+            help="number of propagation rounds")
+    parser.add_argument("--n-epochs", type=int, default=6000,
+            help="number of minimum training epochs")
+    parser.add_argument("-d", "--dataset", type=str, default="FB15k-237",
+            help="dataset to use")
+    parser.add_argument("--eval-batch-size", type=int, default=500,
+            help="batch size when evaluating")
+    parser.add_argument("--eval-protocol", type=str, default="filtered",
+            help="type of evaluation protocol: 'raw' or 'filtered' mrr")
+    parser.add_argument("--regularization", type=float, default=0.01,
+            help="regularization weight")
+    parser.add_argument("--grad-norm", type=float, default=1.0,
+            help="norm to clip gradient to")
+    parser.add_argument("--graph-batch-size", type=int, default=30000,
+            help="number of edges to sample in each iteration")
+    parser.add_argument("--graph-split-size", type=float, default=0.5,
+            help="portion of edges used as positive sample")
+    parser.add_argument("--negative-sample", type=int, default=10,
+            help="number of negative samples per positive sample")
+    parser.add_argument("--evaluate-every", type=int, default=500,
+            help="perform evaluation every n epochs")
+    parser.add_argument("--edge-sampler", type=str, default="neighbor",
+            help="type of edge sampler: 'uniform' or 'neighbor'")
+
+    args = parser.parse_args()
+    print(args)
+    main(args)
---
+++ code/utils/sample_utils.py
@@ -0,0 +1,225 @@
+"""
+Utility functions for link prediction
+Most code is adapted from authors' implementation of RGCN link prediction:
+https://github.com/MichSchli/RelationPrediction
+
+"""
+from concurrent.futures.thread import ThreadPoolExecutor
+from multiprocessing import Queue, Manager
+from multiprocessing import Lock
+
+import numpy as np
+import tensorflow as tf
+import dgl
+import threading
+from queue import PriorityQueue
+from multiprocessing import Pool
+#######################################################################
+#
+# Utility function for building training and testing graphs
+#
+#######################################################################
+
+def get_adj_and_degrees(num_nodes, triplets):
+    """ Get adjacency list and degrees of the graph
+    """
+    adj_list = [[] for _ in range(num_nodes)]
+    for i,triplet in enumerate(triplets):
+        adj_list[triplet[0]].append([i, triplet[2]])
+        adj_list[triplet[2]].append([i, triplet[0]])
+
+    degrees = np.array([len(a) for a in adj_list])
+    adj_list = [np.array(a) for a in adj_list]
+    return adj_list, degrees
+
+def sample_edge_neighborhood(adj_list, degrees, n_triplets, sample_size):
+    """Sample edges by neighborhood expansion.
+
+    This guarantees that the sampled edges form a connected graph, which
+    may help deeper GNNs that require information from more than one hop.
+    """
+    edges = np.zeros((sample_size), dtype=np.int32)
+
+    #initialize
+    sample_counts = np.array([d for d in degrees])
+    picked = np.array([False for _ in range(n_triplets)])
+    seen = np.array([False for _ in degrees])
+
+    for i in range(0, sample_size):
+        weights = sample_counts * seen
+
+        if np.sum(weights) == 0:
+            weights = np.ones_like(weights)
+            weights[np.where(sample_counts == 0)] = 0
+
+        probabilities = (weights) / np.sum(weights)
+        chosen_vertex = np.random.choice(np.arange(degrees.shape[0]),
+                                         p=probabilities)
+        chosen_adj_list = adj_list[chosen_vertex]
+        seen[chosen_vertex] = True
+
+        chosen_edge = np.random.choice(np.arange(chosen_adj_list.shape[0]))
+        chosen_edge = chosen_adj_list[chosen_edge]
+        edge_number = chosen_edge[0]
+
+        while picked[edge_number]:
+            chosen_edge = np.random.choice(np.arange(chosen_adj_list.shape[0]))
+            chosen_edge = chosen_adj_list[chosen_edge]
+            edge_number = chosen_edge[0]
+
+        edges[i] = edge_number
+        other_vertex = chosen_edge[1]
+        picked[edge_number] = True
+        sample_counts[chosen_vertex] -= 1
+        sample_counts[other_vertex] -= 1
+        seen[other_vertex] = True
+
+    return edges
+
+def sample_edge_uniform(adj_list, degrees, n_triplets, sample_size):
+    """Sample edges uniformly from all the edges."""
+    all_edges = np.arange(n_triplets)
+    return np.random.choice(all_edges, sample_size, replace=False)
+
+def generate_sampled_graph_and_labels(triplets, sample_size, split_size,
+                                      num_rels, adj_list, degrees,
+                                      negative_rate, sampler="uniform"):
+    """Get training graph and signals
+    First perform edge neighborhood sampling on graph, then perform negative
+    sampling to generate negative samples
+    """
+    # perform edge neighbor sampling
+    if sampler == "uniform":
+        edges = sample_edge_uniform(adj_list, degrees, len(triplets), sample_size)
+    elif sampler == "neighbor":
+        edges = sample_edge_neighborhood(adj_list, degrees, len(triplets), sample_size)
+    else:
+        raise ValueError("Sampler type must be either 'uniform' or 'neighbor'.")
+
+    # relabel nodes to have consecutive node ids
+    edges = triplets[edges]
+    src, rel, dst = edges.transpose()
+    uniq_v, edges = np.unique((src, dst), return_inverse=True)
+    src, dst = np.reshape(edges, (2, -1))
+    relabeled_edges = np.stack((src, rel, dst)).transpose()
+
+    # negative sampling
+    samples, labels = negative_sampling(relabeled_edges, len(uniq_v),
+                                        negative_rate)
+
+    # further split graph, only half of the edges will be used as graph
+    # structure, while the rest half is used as unseen positive samples
+    split_size = int(sample_size * split_size)
+    graph_split_ids = np.random.choice(np.arange(sample_size),
+                                       size=split_size, replace=False)
+    src = src[graph_split_ids]
+    dst = dst[graph_split_ids]
+    rel = rel[graph_split_ids]
+
+    # build DGL graph
+    # print("# sampled nodes: {}".format(len(uniq_v)))
+    # print("# sampled edges: {}".format(len(src) * 2))
+
+    g, rel, norm = build_graph_from_triplets(len(uniq_v), num_rels,
+                                             (src, rel, dst))
+    return g, uniq_v, rel, norm, samples, labels
+
+def comp_deg_norm(g):
+    g = g.local_var()
+    in_deg = g.in_degrees(range(g.number_of_nodes())).float().numpy()
+    norm = 1.0 / in_deg
+    norm[np.isinf(norm)] = 0
+    return norm
+
+def build_graph_from_triplets(num_nodes, num_rels, triplets):
+    """ Create a DGL graph. The graph is bidirectional because RGCN authors
+        use reversed relations.
+        This function also generates edge type and normalization factor
+        (reciprocal of node incoming degree)
+    """
+    g = dgl.DGLGraph()
+    g.add_nodes(num_nodes)
+    src, rel, dst = triplets
+    src, dst = np.concatenate((src, dst)), np.concatenate((dst, src))
+    rel = np.concatenate((rel, rel + num_rels))
+    edges = sorted(zip(dst, src, rel))
+    dst, src, rel = np.array(edges).transpose()
+    g.add_edges(src, dst)
+    norm = comp_deg_norm(g)
+    # print("# nodes: {}, # edges: {}".format(num_nodes, len(src)))
+    return g, rel, norm
+
+def build_test_graph(num_nodes, num_rels, edges):
+    src, rel, dst = edges.transpose()
+    print("Test graph:")
+    return build_graph_from_triplets(num_nodes, num_rels, (src, rel, dst))
+
+def negative_sampling(pos_samples, num_entity, negative_rate):
+    size_of_batch = len(pos_samples)
+    num_to_generate = size_of_batch * negative_rate
+    neg_samples = np.tile(pos_samples, (negative_rate, 1))
+    labels = np.zeros(size_of_batch * (negative_rate + 1), dtype=np.float32)
+    labels[: size_of_batch] = 1
+    values = np.random.randint(num_entity, size=num_to_generate)
+    choices = np.random.uniform(size=num_to_generate)
+    subj = choices > 0.5
+    obj = choices <= 0.5
+    neg_samples[subj, 0] = values[subj]
+    neg_samples[obj, 2] = values[obj]
+
+    return np.concatenate((pos_samples, neg_samples)), labels
+class MultiThreadSamplerPool(threading.Thread) :
+    def __init__(self, train_data, graph_batch_size, graph_split_size,
+                num_rels, adj_list, degrees, negative_sample,
+                edge_sampler, max_runtime ,max_worker = 16, max_seq = 128):
+        super().__init__()
+        # self.executor = ThreadPoolExecutor(max_worker)
+        # self.lock = Lock()
+        # self.q = Queue(maxsize=max_seq)
+        self.max_runtime = max_runtime
+        self.data = train_data
+        self.graph_batch_size = graph_batch_size
+        self.graph_split_size = graph_split_size
+        self.num_rels = num_rels
+        self.adj_list = adj_list
+        self.degrees = degrees
+        self.negative_sample = negative_sample
+        self.edge_sampler = edge_sampler
+        self.max_worker = max_worker
+        self.manager = Manager()
+
+        self.setDaemon(True)
+        # 父进程创建Queue，并传给各个子进程：
+        self.q = self.manager.Queue(maxsize=max_seq)
+        self.ctx = self.manager.Lock()
+        self.pool = Pool(processes=self.max_worker)
+        self.count = 0
+        self.start()
+
+    def run(self):
+        while self.count <= self.max_runtime:
+            while self.q.full() :
+                pass
+            # task = MultiThreadSampler(self.data, self.graph_batch_size, self.graph_split_size,
+            #                           self.num_rels, self.adj_list, self.degrees, self.negative_sample,
+            #                           self.edge_sampler, self.q, self.lock)
+            self.pool.apply_async(self.process, args=(self.q, self.data, self.graph_batch_size, self.graph_split_size,
+                                               self.num_rels, self.adj_list, self.degrees, self.negative_sample,
+                                               self.edge_sampler))
+            self.count += 1
+            # print(self.q.qsize())
+            # self.q.put(ret.get())
+            # self.q.put(ret.get())
+
+    def get(self):
+        ret = self.q.get()
+        return ret
+
+    @staticmethod
+    def process(q, triplets, sample_size, split_size,
+                                      num_rels, adj_list, degrees,
+                                      negative_rate, sampler="uniform") :
+        q.put(generate_sampled_graph_and_labels(
+            triplets, sample_size, split_size,
+            num_rels, adj_list, degrees,
+            negative_rate, sampler))
