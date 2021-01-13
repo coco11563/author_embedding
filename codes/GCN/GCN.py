7001b1c49b8e4d3f7ba202444aa27840d6564996
coco11563@yeah.net <coco11563@yeah.net>
2021-01-13 17:44:06 +0800

    完成randomwlk dataloader

--- codes/GCN/GCN.py
+++ codes/GCN/GCN.py
@@ -1,9 +1,10 @@
+import numpy
 import torch
 import torch.nn as nn
 from dgl import DGLError
 from dgl import function as fn
 from dgl.utils import expand_as_pair
-
+import torch.nn.functional as Fn
 
 class EncoderRoGCN(nn.Module):
     def __init__(self, in_features, embedding_range, hidden_layer=2,
@@ -130,6 +131,178 @@ class EncoderRGCNLayer(nn.Module):
             if self.activation is not None:
                 rst = self.activation(rst)
             return rst
+class ScorePredictor(nn.Module):
+    def __init__(self, dist_func, sw = None):
+        super(ScorePredictor, self).__init__()
+        self.dist_func = dist_func
+        self.sw = sw
+
+    def forward(self, edge_subgraph, x):
+        with edge_subgraph.local_scope():
+            edge_subgraph.ndata['x'] = x
+            edge_subgraph.apply_edges(self.edge_func)
+            return edge_subgraph.edata['score']
+
+    def edge_func(self, edges):
+        head = edges.src['x']
+        tail = edges.dst['x']
+        score = self.dist_func(head, tail, mode='single')
+        if self.sw is not None :
+            sw = edges.data[self.sw]
+            score = score * sw
+        # TODO: check if there exists minus sign and if gamma should be used here(jin)
+        return {'score': score}
+
+def dist_func(head, tail) :
+    score = head * tail
+    score = score.norm(dim=0)
+    return score
+
+class GCN(nn.Module) :
+    def __init__(self, whole_graph, nentity, nrelation, hidden_dim, graph_layer_num=2,
+                 ent_ini = None, ent_drop=0, rel_drop=0, self_loop=True, graph_activation=None, freeze = False):
+        super(GCN, self).__init__()
+        self.nentity = nentity
+        self.nrelation = nrelation
+        self.hidden_dim = hidden_dim
+        self.graph_layer_num = graph_layer_num
+
+        self.whole_graph = whole_graph
+
+
+        self.embedding_range = nn.Parameter(
+            torch.Tensor([(self.gamma.item() + self.epsilon) / hidden_dim]),
+            requires_grad=False
+        )
+        # entity embedding init :
+        if ent_ini is None:
+            self.entity_embedding = nn.Parameter(torch.zeros(nentity, self.entity_dim))
+            nn.init.xavier_uniform_(
+                tensor=self.entity_embedding,
+                gain=graph_activation
+            )
+            self.entity_dim = hidden_dim
+            self.relation_dim = hidden_dim
+        else:
+            embed_li = []
+            hidden_dim = len(ent_ini[0])
+            for i in range(whole_graph.number_of_nodes()):
+                embed_li.append(ent_ini[i])
+            embed_li = numpy.asarray(embed_li)
+            embed_li = torch.from_numpy(embed_li).float()
+            self.entity_dim = hidden_dim
+            self.relation_dim = hidden_dim
+            self.ent_ini = nn.Embedding.from_pretrained(embed_li, freeze=freeze)
+        self.relation_embedding = nn.Parameter(torch.zeros(nrelation, self.relation_dim))
+        nn.init.xavier_uniform_(
+            tensor=self.relation_embedding,
+            gain=graph_activation
+        )
+        if graph_activation == 'relu':
+            act_fun = Fn.relu
+        elif graph_activation == 'gelu':
+            act_fun = Fn.gelu
+        elif graph_activation == 'sigmoid':
+            act_fun = Fn.sigmoid
+        elif graph_activation == 'elu':
+            act_fun = Fn.elu
+        elif graph_activation == 'glu':
+            act_fun = Fn.glu
+        elif graph_activation == 'none':
+            act_fun = None
+        else:
+            raise ValueError(
+                'the graph activation function should be [relu gelu sigmoid elu glu or none] but instead of {}'
+                .format(graph_activation))
+
+        self.whole_graph.ndata['feature'] = self.entity_embedding
+        self.Encoder = EncoderRoGCN(self.entity_dim, self.embedding_range, self.hidden_dim,
+                                    activation=act_fun,
+                                    self_loop=self_loop, ent_drop=ent_drop, rel_drop=rel_drop)
+        self.predictor = ScorePredictor(dist_func)
+
+
+    def compute_loss(self, pos_score, neg_score, args):
+        print(neg_score.shape)
+        # Margin loss
+        if args.adversarial_temperature != 0 :
+            negative_score = (Fn.softmax(neg_score * args.adversarial_temperature).detach()
+                          * Fn.logsigmoid(-neg_score)).sum()
+        else:
+            negative_score = Fn.logsigmoid(-neg_score).sum()
+        positive_score = Fn.logsigmoid(pos_score).squeeze()
+
+        positive_sample_loss = - positive_score.mean()
+        negative_sample_loss = - negative_score.mean()
+        # neg_loss, pos_loss = (self.margin_loss(negative_score.view(n_edges, -1), pos_score[etype].unsqueeze(1)))
+        neg_loss = negative_sample_loss
+        pos_loss = positive_sample_loss
+        return pos_loss, neg_loss
+
+    def get_loss(self, positive_graph, negative_graph, blocks, x, args):
+        x = self.Encoder(blocks, x, self.relation_embedding)
+        positive_score, negative_score = self.pred(positive_graph, negative_graph, x)
+        return self.compute_loss(positive_score, negative_score, args)
+
+    def forward(self, blocks, x):
+        '''
+        Forward function that calculate the score of a batch of triples.
+        combine negative graph score => negative score / positive score => positive score
+        '''
+        x = self.Encoder(blocks, x, self.relation_embedding)
+        return x
+
+    def pred(self, positive_graph, negative_graph, x):
+        positive_score = self.predictor(positive_graph, x)
+        negative_score = self.predictor(negative_graph, x)
+        return positive_score, negative_score
+
+    @staticmethod
+    def train_step(model, optimizer, iterator, args):
+        '''
+        A single train step. Apply back-propation and return the loss
+        '''
+        model.train()
+
+        input_nodes, positive_graph, negative_graph, blocks = next(iterator)
+        if args.cuda:
+            blocks = [b.to(torch.device('cuda:0')) for b in blocks]
+            positive_graph = positive_graph.to(torch.device('cuda:0'))
+            negative_graph = negative_graph.to(torch.device('cuda:0'))
+
+        optimizer.zero_grad()
+        input_features = blocks[0].srcdata['feature']
+        pos_loss, neg_loss = model.get_loss(positive_graph, negative_graph, blocks, input_features, args)
+        # if args.subsampling_weight:
+        #     neg_subsampling_weight = negative_graph.edata['sw']
+        #     pos_subsampling_weight = positive_graph.edata['sw']
+        #     positive_sample_loss = - (subsampling_weight * positive_score).sum() / subsampling_weight.sum()
+        #     negative_sample_loss = - (subsampling_weight * negative_score).sum() / subsampling_weight.sum()
+        # else :
+        loss = (pos_loss + neg_loss) / 2
+        if args.regularization != 0.0:
+            # Use L3 regularization for ComplEx and DistMult
+            regularization = args.regularization * (
+                    args.ent_regularization * model.entity_embedding.norm(p=3) ** 3 +
+                    args.rel_regularization * model.relation_embedding.norm(p=3).norm(p=3) ** 3
+            )
+            loss = loss + regularization
+            regularization_log = {'regularization': regularization.item()}
+        else:
+            regularization_log = {}
+
+        loss.backward()
+
+        optimizer.step()
+
+        log = {
+            **regularization_log,
+            'positive_sample_loss': pos_loss.item(),
+            'negative_sample_loss': neg_loss.item(),
+            'loss': loss.item()
+        }
+
+        return log
 
 def main(args):
     # load graph data
--- codes/utils/node_samplr/randomwalk.py
+++ codes/utils/node_samplr/randomwalk.py
@@ -5,16 +5,19 @@ import dgl.sampling.randomwalks as walk
 import numpy
 import torch
 from dgl import backend as F
-from dgl.dataloading import DataLoader, Mapping, utils, Collator, NID
-from dgl.dataloading.pytorch import _NodeDataLoaderIter, _pop_blocks_storage
+from dgl.dataloading import DataLoader, Mapping, utils, Collator, NID, distributed, subgraph, sampling, BlockSampler, \
+    EID, transform
+from dgl.dataloading.dataloader import _tensor_or_dict_to_numpy, _locate_eids_to_exclude, assign_block_eids
+from dgl.dataloading.pytorch import _pop_blocks_storage, _restore_blocks_storage
 from dgl.distributed import DistGraph
+from torch.utils.data import WeightedRandomSampler
 
 
-class RandomWalkSampler :
+class RandomWalkSampler:
     def __init__(self, tuples, base_num,
                  metapath=None, length=None, prob=None,
-                 restart_prob=None, windows = 2,
-                 true_tuple = None, neg_num = 0):
+                 restart_prob=None, windows=2,
+                 true_tuple=None, neg_num=0):
         self.metapath = metapath
         self.length = length
         self.prob = prob
@@ -23,38 +26,50 @@ class RandomWalkSampler :
         self.neg_num = neg_num
         self.true_tuple = self.get_true_head_and_tail(true_tuple)
         self.count = self.count_frequency(tuples, base_num)
+
     """
     目前的问题主要在于 采样时会采很多次入度多的点
     """
+
     def sampler(self, g, nids):
         traces, type = walk.random_walk(g, nodes=nids,
                                         metapath=self.metapath, length=self.length,
                                         prob=self.prob, restart_prob=self.restart_prob)
+        print('trace is', traces)
         num_nodes = g.number_of_nodes()
         tuples = []
         labels = []
         node_set = set()
         subsampling_ws = []
-        for path in traces.numpy() :
+        for path in traces.numpy():
             srcs, dsts, subsampling_w = self.trace_sampler(path, node_set)
-            neg_src, neg_dst, neg_subsampling_w = self.negative_sampling(srcs, dsts, num_nodes, node_set)
-            srcs = torch.from_numpy(numpy.array(srcs))
-            dsts = torch.from_numpy(numpy.array(dsts))
-            pos_sample = torch.cat((srcs.reshape(-1, 1),dsts.reshape(-1, 1)), 1)
-            neg_sample = torch.cat((neg_src.reshape(-1, 1), neg_dst.reshape(-1, 1)), 1)
-            pos_label = torch.ones_like(srcs) # n
-            neg_label = torch.ones_like(neg_src) * -1
-            label = torch.cat((pos_label, neg_label)) # n
-            tuple = torch.cat((pos_sample, neg_sample))
-            sub_w = torch.cat((subsampling_w, neg_subsampling_w))
+            if self.neg_num > 0 :
+                neg_src, neg_dst, neg_subsampling_w = self.negative_sampling(srcs, dsts, num_nodes, node_set)
+                srcs = torch.from_numpy(numpy.array(srcs))
+                dsts = torch.from_numpy(numpy.array(dsts))
+                pos_sample = torch.cat((srcs.reshape(-1, 1), dsts.reshape(-1, 1)), 1)
+                neg_sample = torch.cat((neg_src.reshape(-1, 1), neg_dst.reshape(-1, 1)), 1)
+                pos_label = torch.ones_like(srcs)  # n
+                neg_label = torch.ones_like(neg_src) * -1
+                label = torch.cat((pos_label, neg_label))  # n
+                tuple = torch.cat((pos_sample, neg_sample))
+                sub_w = torch.cat((subsampling_w, neg_subsampling_w))
+            else :
+                srcs = torch.from_numpy(numpy.array(srcs))
+                dsts = torch.from_numpy(numpy.array(dsts))
+                tuple = torch.cat((srcs.reshape(-1, 1), dsts.reshape(-1, 1)), 1)
+                label = torch.ones_like(srcs)  # n
+                sub_w = subsampling_w
             tuples.append(tuple)
             labels.append(label)
             subsampling_ws.append(sub_w)
-        return tuples, labels, subsampling_ws, torch.from_numpy(numpy.array(list(node_set)))
+        return torch.cat(tuples), torch.cat(labels), torch.cat(subsampling_ws), torch.from_numpy(numpy.array(list(node_set)))
+
     """
     输入 一组 trace
     返回 对应的 src dst 以及sub sampling weight
     """
+
     def trace_sampler(self, traces, node_set):
         srcs = []
         dsts = []
@@ -66,7 +81,7 @@ class RandomWalkSampler :
                 # sample every window_size
                 window_s = i + 1
                 next_indice = start + window_s
-                if next_indice <= self.length :
+                if next_indice <= self.length:
                     dst = traces[next_indice]
                     subsampling_weight = self.count[src] + self.count[- dst - 1]
                     subsampling_weight = torch.sqrt(1 / torch.Tensor([subsampling_weight]))
@@ -84,14 +99,16 @@ class RandomWalkSampler :
     ---
     返回 负采样/子采样权值
     """
+
     def negative_sampling(self, nsrcs, ndsts, node_limit, node_set):
         subsampling_w = []
         neg_sample = None
         for indice, src in enumerate(nsrcs):
             negative_sample_size = 0
             negative_sample_list = []
-            subsampling_weight = self.count[src] + self.count[ - ndsts[indice] - 1]
+            subsampling_weight = self.count[src] + self.count[- ndsts[indice] - 1]
             subsampling_weight = torch.sqrt(1 / torch.Tensor([subsampling_weight]))
+
             while negative_sample_size < self.neg_num:
                 # TODO 这里的负采样是全局负采样，对于有关系的需要考虑 metapath
                 # 这里都是tail负采样
@@ -106,6 +123,7 @@ class RandomWalkSampler :
                 negative_sample_list.append(negative_sample)
                 negative_sample_size += negative_sample.size
                 subsampling_w.append(subsampling_weight)
+
             negative_sample = numpy.concatenate(negative_sample_list)[:self.neg_num]
 
             if neg_sample is None:
@@ -115,12 +133,13 @@ class RandomWalkSampler :
         subsampling_w = torch.cat(subsampling_w, dim=-1)
         subsampling_w = F.repeat(subsampling_w, self.neg_num, 0)
         pos_src = torch.tensor(nsrcs, dtype=torch.long)
-        pos_src = F.repeat(pos_src,self.neg_num, 0)
+        pos_src = F.repeat(pos_src, self.neg_num, 0)
         dsts = torch.from_numpy(neg_sample)
-        for i in dsts :
+        for i in dsts:
             num = i.item()
             node_set.add(num)
         return pos_src, dsts, subsampling_w
+
     @staticmethod
     def count_frequency(tuples, base_num):
         count = {}
@@ -139,12 +158,98 @@ class RandomWalkSampler :
     @staticmethod
     def get_true_head_and_tail(true_trace):
         ret = dict()
-        for k,v in true_trace.items() :
+        for k, v in true_trace.items():
             ret[k] = numpy.array(list(v))
         return ret
 
+
+class RandomWalkMultiLayerNeighborSampler(BlockSampler):
+    def __init__(self, fanouts, tuples, base_num, replace=False, return_eids=False,
+                 metapath=None, length=None, prob=None,
+                 restart_prob=None, windows=2,
+                 true_tuple=None, neg_num=0):
+        super().__init__(len(fanouts), return_eids)
+        self.random_walk_sampler = RandomWalkSampler(tuples, base_num, metapath=metapath, length=length, prob=prob,
+                 restart_prob=restart_prob, windows=windows,
+                 true_tuple=true_tuple, neg_num=neg_num)
+
+        self.fanouts = fanouts
+        self.replace = replace
+
+    def sample_frontier(self, block_id, g, seed_nodes):
+        fanout = self.fanouts[block_id]
+        if isinstance(g, distributed.DistGraph):
+            if fanout is None:
+                # TODO(zhengda) There is a bug in the distributed version of in_subgraph.
+                # let's use sample_neighbors to replace in_subgraph for now.
+                frontier = distributed.sample_neighbors(g, seed_nodes, -1, replace=False)
+            else:
+                frontier = distributed.sample_neighbors(g, seed_nodes, fanout, replace=self.replace)
+        else:
+            if fanout is None:
+                frontier = subgraph.in_subgraph(g, seed_nodes)
+            else:
+                frontier = sampling.sample_neighbors(g, seed_nodes, fanout, replace=self.replace)
+        return frontier
+
+    def sample_blocks(self, g, seed_nodes, exclude_eids=None) :
+        print('seed is ', seed_nodes)
+        blocks = []
+        exclude_eids = (
+            _tensor_or_dict_to_numpy(exclude_eids) if exclude_eids is not None else None)
+        tuples, labels, subsampling_ws, seed_nodes = self.random_walk_sampler.sampler(g, seed_nodes)
+        for block_id in reversed(range(self.num_layers)):
+            frontier = self.sample_frontier(block_id, g, seed_nodes)
+
+            # Removing edges from the frontier for link prediction training falls
+            # into the category of frontier postprocessing
+            if exclude_eids is not None:
+                parent_eids = frontier.edata[EID]
+                parent_eids_np = _tensor_or_dict_to_numpy(parent_eids)
+                located_eids = _locate_eids_to_exclude(parent_eids_np, exclude_eids)
+                if not isinstance(located_eids, Mapping):
+                    # (BarclayII) If frontier already has a EID field and located_eids is empty,
+                    # the returned graph will keep EID intact.  Otherwise, EID will change
+                    # to the mapping from the new graph to the old frontier.
+                    # So we need to test if located_eids is empty, and do the remapping ourselves.
+                    if len(located_eids) > 0:
+                        frontier = transform.remove_edges(frontier, located_eids)
+                        frontier.edata[EID] = F.gather_row(parent_eids, frontier.edata[EID])
+                else:
+                    # (BarclayII) remove_edges only accepts removing one type of edges,
+                    # so I need to keep track of the edge IDs left one by one.
+                    new_eids = parent_eids.copy()
+                    for k, v in located_eids.items():
+                        if len(v) > 0:
+                            frontier = transform.remove_edges(frontier, v, etype=k)
+                            new_eids[k] = F.gather_row(parent_eids[k], frontier.edges[k].data[EID])
+                    frontier.edata[EID] = new_eids
+
+            block = transform.to_block(frontier, seed_nodes)
+
+            if self.return_eids:
+                assign_block_eids(block, frontier)
+
+            seed_nodes = {ntype: block.srcnodes[ntype].data[NID] for ntype in block.srctypes}
+
+            # Pre-generate CSR format so that it can be used in training directly
+            block.create_formats_()
+            blocks.insert(0, block)
+        return tuples, labels, subsampling_ws, blocks
+
+class RandomWalkMultiLayerFullNeighborSampler(RandomWalkMultiLayerNeighborSampler):
+    def __init__(self, n_layers, tuples, base_num, replace=False, return_eids=False,
+                 metapath=None, length=None, prob=None,
+                 restart_prob=None, windows=2,
+                 true_tuple=None, neg_num=0
+                 ):
+        super().__init__([None] * n_layers, tuples, base_num, replace=replace, return_eids=return_eids,
+                 metapath=metapath, length=length, prob=prob,
+                 restart_prob=restart_prob, windows=windows,
+                 true_tuple=true_tuple, neg_num=neg_num)
+
 # remove dist graph support
-class NodeCollator(Collator):
+class RandomWalkNodeCollator(Collator):
     def __init__(self, g, nids, block_sampler):
         self.g = g
         self._is_distributed = isinstance(g, DistGraph)
@@ -155,6 +260,7 @@ class NodeCollator(Collator):
         self.nids = nids
         self.block_sampler = block_sampler
 
+        # for heterogeneous graph
         if isinstance(nids, Mapping):
             self._dataset = utils.FlattenedDict(nids)
         else:
@@ -173,16 +279,16 @@ class NodeCollator(Collator):
         else:
             items = utils.prepare_tensor(self.g, items, 'items')
 
-        blocks = self.block_sampler.sample_blocks(self.g, items)
+        # items is the chosen id
+        tuples, labels, subsampling_ws, blocks = self.block_sampler.sample_blocks(self.g, items)
         output_nodes = blocks[-1].dstdata[NID]
         input_nodes = blocks[0].srcdata[NID]
 
-        return input_nodes, output_nodes, blocks
+        return tuples, labels, subsampling_ws, input_nodes, output_nodes, blocks
 
 
 class RandomWalkNodeDataLoader:
-    collator_arglist = inspect.getfullargspec(NodeCollator).args
-
+    collator_arglist = inspect.getfullargspec(RandomWalkNodeCollator).args
     def __init__(self, g, nids, block_sampler, **kwargs):
         collator_kwargs = {}
         dataloader_kwargs = {}
@@ -193,11 +299,11 @@ class RandomWalkNodeDataLoader:
                 dataloader_kwargs[k] = v
 
         assert not isinstance(g, DistGraph)
-
+        # block sampler is the neighbor sampler
         self.collator = _NodeCollator(g, nids, block_sampler, **collator_kwargs)
         self.dataloader = DataLoader(self.collator.dataset,
-                                     collate_fn=self.collator.collate,
-                                     **dataloader_kwargs)
+                                         collate_fn=self.collator.collate,
+                                         **dataloader_kwargs)
         self.is_distributed = False
 
     def __iter__(self):
@@ -212,17 +318,40 @@ class RandomWalkNodeDataLoader:
         """Return the number of batches of the data loader."""
         return len(self.dataloader)
 
-class _NodeCollator(NodeCollator):
+
+class _NodeCollator(RandomWalkNodeCollator):
     def collate(self, items):
-        input_nodes, output_nodes, blocks = super().collate(items)
+        tuples, labels, subsampling_ws, input_nodes, output_nodes, blocks = super().collate(items)
         _pop_blocks_storage(blocks, self.g)
-        return input_nodes, output_nodes, blocks
+        return tuples, labels, subsampling_ws, input_nodes, output_nodes, blocks
+
+
+class _NodeDataLoaderIter:
+    def __init__(self, node_dataloader):
+        self.node_dataloader = node_dataloader
+        self.iter_ = iter(node_dataloader.dataloader)
+
+    def __next__(self):
+        tuples, labels, subsampling_ws, input_nodes, output_nodes, blocks = next(self.iter_)
+        _restore_blocks_storage(blocks, self.node_dataloader.collator.g)
+        return tuples, labels, subsampling_ws, input_nodes, output_nodes, blocks
 
 if __name__ == '__main__':
     u, v = torch.tensor([0, 0, 0, 1, 1, 2, 3, 3]), torch.tensor([1, 2, 3, 3, 0, 0, 0, 1])
     g = dgl.graph((u, v))
+    g.ndata['p'] = 1 / g.out_degrees()
     true_trace = {2: {0}, 0: {1, 2, 3}, 1: {3, 0}, 3: {0, 1}}
     tuples = torch.cat((u.view((-1, 1)), v.view(-1, 1)), 1)
     base_num = 1
-    smp = RandomWalkSampler(tuples.numpy(), base_num, neg_num=4, true_tuple=true_trace, length=4)
-    print(smp.sampler(g, [0,1]))
+    sampler = RandomWalkMultiLayerNeighborSampler([None], tuples.numpy(), base_num, neg_num=0, true_tuple=true_trace, length=2, windows=1)
+    train_nid = g.nodes()
+    dataloader = RandomWalkNodeDataLoader(
+        g, train_nid, sampler,
+        batch_size=1, shuffle=True, drop_last=False, num_workers=4)
+    for tuple, label, subsampling_ws, input_nodes, output_nodes, blocks in dataloader:
+        print(tuple)
+        print(label)
+        print(input_nodes)
+        print(output_nodes)
+        print(blocks)
+        break
